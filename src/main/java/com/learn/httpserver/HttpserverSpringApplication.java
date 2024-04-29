ef55303bc371e960c6cff587ad6d0305f9c350ec
PeterLu <1649675756@qq.com>
2024-04-29 11:17:29 +0800

    first commit

---
+++ src/main/java/com/learn/httpserver/HttpserverSpringApplication.java
@@ -0,0 +1,13 @@
+package com.learn.httpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpserverSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpserverSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/httpserver/HttpserverSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.httpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpserverSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
